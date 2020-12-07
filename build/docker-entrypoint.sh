e74673269a752c62894d6bd7f2049e295caa653e
kosehy@gmail.com <kosehy@gmail.com>
2020-12-08 02:11:08 +0900

    Add youngcart

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-# 그누보드 최신버전 자동설치 스크립트 시작
-if [ ! -e gnuboard ]; then
-mkdir gnuboard \
-&& cd gnuboard \
-&& curl -o gnuboard.tar.gz -fSL https://github.com/gnuboard/gnuboard5/tarball/master \
-&& tar -xf gnuboard.tar.gz -C ./ --strip-components=1 \
-&& rm gnuboard.tar.gz && mkdir data \
+# 영카트 최신버전 자동설치 스크립트 시작
+if [ ! -e youngcart ]; then
+mkdir youngcart \
+&& cd youngcart \
+&& curl -o youngcart.tar.gz -fSL https://github.com/gnuboard/youngcart5/tarball/master \
+&& tar -xf youngcart.tar.gz -C ./ --strip-components=1 \
+&& rm youngcart.tar.gz && mkdir data \
 && chown -R www-data:www-data ./ \
 && chmod -R 777 data \
 && sed -i "s/'utf8'/'utf8mb4'/g" config.php \
